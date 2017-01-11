2783a8eb1631b72734746f6ed16e9845558a2354
Kotaimen <kotaimen.c@gmail.com>
2017-01-11 11:24:09 +0800

    Add canned policy cli options.

--- awscfncli/cli.py
+++ awscfncli/cli.py
@@ -77,6 +77,59 @@ def pretty_print_stack(stack, detail=0):
         echo_pair('%s: ' % t['Key'], t['Value'], indent=2)
 
 
+CANNED_STACK_POLICIES = {
+    'ALLOW_ALL': '''
+{
+  "Statement" : [
+    {
+      "Effect" : "Allow",
+      "Action" : "Update:*",
+      "Principal": "*",
+      "Resource" : "*"
+    }
+  ]
+}
+''',
+    'ALLOW_MODIFY': '''
+{
+  "Statement" : [
+    {
+      "Effect" : "Deny",
+      "Action" : ["Update:Replace", "Update:Delete"]
+      "Principal": "*",
+      "Resource" : "*"
+    }
+  ]
+}
+''',
+    'DENY_DELETE': '''
+{
+  "Statement" : [
+    {
+      "Effect" : "Deny",
+      "Action" : "Update:Delete",
+      "Principal": "*",
+      "Resource" : "*"
+    }
+  ]
+}
+''',
+    'DENY_ALL': '''
+{
+  "Statement" : [
+    {
+      "Effect" : "Deny",
+      "Action" : "Update:*",
+      "Principal": "*",
+      "Resource" : "*"
+    }
+  ]
+}
+''',
+
+}
+
+
 #
 # Click CLI
 #
@@ -89,40 +142,63 @@ def cli(ctx):
 
 @cli.command()
 @click.argument('config_file', type=click.Path(exists=True))
-@click.option('--no-wait/--wait', default=False,
-              help='Wait and print stack events until stack creation is complete.')
+@click.option('--no-wait', is_flag=True, default=False,
+              help='Wait and print stack events until stack delete is complete.')
 @click.option('--on-failure',
               type=click.Choice(['DO_NOTHING', 'ROLLBACK', 'DELETE']),
               default=None,
               help='Determines what action will be taken if stack creation '
                    'fails. This must be one of: DO_NOTHING, ROLLBACK, or '
-                   'DELETE. NOTE: set this option overwrites "OnFailure" and '
-                   '"DisableRollback" in the stack configuration file.')
+                   'DELETE. Note setting this option overwrites "OnFailure" '
+                   'and "DisableRollback" in the stack configuration file.')
+@click.option('--canned-policy',
+              type=click.Choice(['ALLOW_MODIFY',
+                                 'DENY_DELETE', 'DENY_ALL', ]),
+              default=None,
+              help='Attach a predefined Stack Policy as StackPolicyBody when '
+                   'deploying the stack.  A Stack Policy controls whether '
+                   'change to stack resources are allowed during stack update.  '
+                   'Valid canned policy are: \b\n'
+                   'DENY_DELETE: Allows modify and replace, denys delete\n'
+                   'ALLOW_MODIFY: Allows modify, denys replace and delete\n'
+                   'DENY_ALL: Denys all updates\n'
+                   'Note setting this option overwrites "PolicyBody" and '
+                   '"PolicyURL" in the stack configuration file.')
 @click.pass_context
 @boto3_exception_handler
-def deploy(ctx, config_file, no_wait, on_failure):
+def deploy(ctx, config_file, no_wait, on_failure, canned_policy):
     """Deploy a new stack using specified stack configuration file"""
+    # load config
     stack_config = load_stack_config(config_file)
     pretty_print_config(stack_config)
     click.echo('Deploying stack...')
 
-    region = stack_config.pop('Region')
-    cfn = boto3.resource('cloudformation', region_name=region)
-
+    # option handling
     if on_failure is not None:
         stack_config.pop('DisableRollback', None)
         stack_config['OnFailure'] = on_failure
 
+    if canned_policy is not None:
+        stack_config.pop('StackPolicyURL', None)
+        stack_config['StackPolicyBody'] = CANNED_STACK_POLICIES[canned_policy]
+
+    # connect to cfn
+    region = stack_config.pop('Region')
+    cfn = boto3.resource('cloudformation', region_name=region)
+
+    # create stack
     stack = cfn.create_stack(**stack_config)
     stack_id = stack.stack_id
-
     pretty_print_stack(stack)
 
+    # exit immediately
     if no_wait:
         return
 
+    # start event tailing
     start_tail_stack_events_daemon(stack, latest_events=0)
 
+    # wait until update complete
     waiter = boto3.client('cloudformation', region_name=region).get_waiter(
         'stack_create_complete')
     waiter.wait(StackName=stack_id)
@@ -132,16 +208,18 @@ def deploy(ctx, config_file, no_wait, on_failure):
 
 @cli.command()
 @click.argument('config_file', type=click.Path(exists=True))
-@click.option('--no-wait/--wait', default=False,
-              help='Wait and print stack events until stack update is complete.')
+@click.option('--no-wait', is_flag=True, default=False,
+              help='Wait and print stack events until stack delete is complete.')
 @click.pass_context
 @boto3_exception_handler
 def delete(ctx, config_file, no_wait):
     """Delete the stack specified in the configuration file"""
+    # load config
     stack_config = load_stack_config(config_file)
     pretty_print_config(stack_config)
     click.echo('Deleting stack...')
 
+    # connect co cfn
     region = stack_config.pop('Region')
     cfn = boto3.resource('cloudformation', region_name=region)
     stack = cfn.Stack(stack_config['StackName'])
@@ -149,13 +227,17 @@ def delete(ctx, config_file, no_wait):
 
     pretty_print_stack(stack)
 
+    # delte the stack
     stack.delete()
 
+    # exit immediately
     if no_wait:
         return
 
+    # start event tailing
     start_tail_stack_events_daemon(stack, latest_events=2)
 
+    # wait until delete complete
     waiter = boto3.client('cloudformation', region_name=region).get_waiter(
         'stack_delete_complete')
     waiter.wait(StackName=stack_id)
@@ -165,16 +247,45 @@ def delete(ctx, config_file, no_wait):
 
 @cli.command()
 @click.argument('config_file', type=click.Path(exists=True))
-@click.option('--no-wait/--wait', default=False,
+@click.option('--no-wait', is_flag=True, default=False,
               help='Wait and print stack events until stack delete is complete.')
+@click.option('--use-previous-template', is_flag=True, default=False,
+              help='Reuse the existing template that is associated with the '
+                   'stack that you are updating.')
+@click.option('--canned-policy',
+              type=click.Choice(['ALLOW_ALL', 'ALLOW_MODIFY',
+                                 'DENY_DELETE', 'DENY_ALL', ]),
+              default=None,
+              help='A predefined Stack Policy as StackPolicyBody when '
+                   'updating the stack.  A Stack Policy controls whether '
+                   'change to stack resources are allowed during stack update.  '
+                   'Valid canned policy are: \b\n'
+                   'ALLOW_ALL: Allows all updates\n'
+                   'DENY_DELETE: Allows modify and replace, denys delete\n'
+                   'ALLOW_MODIFY: Allows modify, denys replace and delete\n'
+                   'DENY_ALL: Denys all updates\n'
+                   'Note setting this option overwrites "PolicyBody" and '
+                   '"PolicyURL" in the stack configuration file.')
+@click.option('--override-policy',
+              type=click.Choice(['ALLOW_ALL', 'ALLOW_MODIFY',
+                                 'DENY_DELETE']),
+              default=None,
+              help='Temporary overriding stack policy during this update.'
+                   'Valid canned policy are: \b\n'
+                   'ALLOW_ALL: Allows all updates\n'
+                   'DENY_DELETE: Allows modify and replace, denys delete\n'
+                   'ALLOW_MODIFY: Allows modify, denys replace and delete\n')
 @click.pass_context
 @boto3_exception_handler
-def update(ctx, config_file, no_wait):
+def update(ctx, config_file, no_wait, use_previous_template,
+           canned_policy, override_policy):
     """Update the stack specified in the configuration file"""
+    # load config
     stack_config = load_stack_config(config_file)
     pretty_print_config(stack_config)
     click.echo('Updating stack...')
 
+    # connect co cfn
     region = stack_config.pop('Region')
     cfn = boto3.resource('cloudformation', region_name=region)
     stack = cfn.Stack(stack_config['StackName'])
@@ -183,21 +294,40 @@ def update(ctx, config_file, no_wait):
     stack_config.pop('DisableRollback', None)
     stack_config.pop('OnFailure', None)
 
+    # update parameters
+    if use_previous_template:
+        stack_config.pop('TemplateBody', None)
+        stack_config.pop('TemplateURL', None)
+        stack_config['UsePreviousTemplate'] = use_previous_template
+
+    if canned_policy is not None:
+        stack_config.pop('StackPolicyURL', None)
+        stack_config['StackPolicyBody'] = CANNED_STACK_POLICIES[canned_policy]
+
+    if override_policy is not None:
+        click.echo('Overriding stack policy during update...')
+        stack_config['StackPolicyDuringUpdateBody'] = \
+            CANNED_STACK_POLICIES[override_policy]
+
     stack_id = stack.stack_id
     pretty_print_stack(stack)
 
+    # update stack
     stack.update(**stack_config)
 
+    # exit immediately
     if no_wait:
         return
 
+    # start event tailing
     start_tail_stack_events_daemon(stack, latest_events=2)
 
+    # wait until update complete
     waiter = boto3.client('cloudformation', region_name=region).get_waiter(
         'stack_update_complete')
     waiter.wait(StackName=stack_id)
 
-    click.echo(click.style('Stack update complete.', fg='greeen'))
+    click.echo(click.style('Stack update complete.', fg='green'))
 
 
 @cli.command()
