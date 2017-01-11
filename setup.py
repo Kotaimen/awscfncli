880a3bad54853b01a743bf5d0922da98e7d27763
ray <gliese.q@gmail.com>
2017-01-11 14:47:23 +0800

    Update package information

--- setup.py
+++ setup.py
@@ -1,25 +1,75 @@
 # -*- encoding: utf-8 -*-
 
+import os
+from setuptools import setup, find_packages
+
 __author__ = 'kotaimen'
 __date__ = '03/01/2017'
 
-from setuptools import setup, find_packages
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
+install_requires = [
+    'six>=1.10.0',
+    'boto3>=1.3',
+    'click>=6.0',
+    'PyYAML>=3.11',
+]
+
+test_requires = ['pytest', 'pytest-cov']
+
+dev_requires = test_requires
 
 setup(
+    # Project Name
     name='awscfncli',
+
+    # Version and description
     version='0.1',
     description='AWS CloudFormation CLI',
-    author='Kotaimen',
-    author_email='kotaimen.c@gmail.com',
+    long_description=long_description,
+
+    # Author details
+    author='Kotaimen, Ray',
+    author_email='kotaimen.c@gmail.com, gliese.q@gmail.com',
+
+    # Project home
     url='https://github.com/Kotaimen/awscfncli',
+
+    # License detail
     license='MIT',
-    install_requires=[
-        'six>=1.10.0',
-        'boto3>=1.3',
-        'click>=6.0',
-        'PyYAML>=3.11',
+
+    # Classification and Keywords
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Operating System :: OS Independent',
+
+        'Development Status :: 2 - Pre-Alpha',
+
+        'Natural Language :: English',
+
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
     ],
+    keywords='awscfncli aws cloudformation development',
+
     packages=find_packages(exclude=('tests',)),
+    install_requires=install_requires,
+    extras_require={
+        'test': test_requires,
+        'dev': dev_requires
+    },
+
     entry_points='''
         [console_scripts]
         cfn=awscfncli.cli:cli
