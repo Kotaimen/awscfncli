3226558e3ab99ffc7b6ebdc8eb5e812cb25d06da
Ray <gliese.q@gmail.com>
2018-03-08 01:49:38 +0800

    Add config interface sample

--- awscfncli/config/config.py
+++ awscfncli/config/config.py
@@ -1,20 +1,61 @@
 # -*- encoding: utf-8 -*-
 
-import six
 import logging
 import yaml
 from collections import namedtuple
 
 log = logging.getLogger(__name__)
 
-CFNFILE_V1 = 1
-CFNFILE_V2 = 2
-
 
 def load(filename):
-    return Config()
+    with open(filename) as fp:
+        config = yaml.safe_load(fp)
+
+    return CfnCliConfig.load(config)
 
 
-class Config(namedtuple('_Config', '')):
-    def validate(self):
-        return False
+class CfnCliConfig(namedtuple('CfnCliConfig', 'version blueprints environments')):
+    CFNFILE_V1 = 1
+    CFNFILE_V2 = 2
+
+    VERSION_SECTION = 'Version'
+    BLUEPRINT_SECTION = 'Blueprints'
+    ENVIRONMENT_SECTION = 'Environments'
+
+    @classmethod
+    def load(cls, config):
+        # load version
+        version = config.get(cls.VERSION_SECTION, cls.CFNFILE_V1)
+
+        # load blueprint into dict
+        blueprint_section = config.get(cls.BLUEPRINT_SECTION, {})
+        blueprints = {}
+        for key, val in blueprint_section:
+            blueprints[key] = Blueprint.load(val)
+
+        # load environment into dict
+        environment_section = config.get(cls.ENVIRONMENT_SECTION, {})
+        environments = {}
+        for key, val in environment_section:
+            environments[key] = Environment.load(val)
+
+        return cls(version, blueprints, environments)
+
+
+class Stack(namedtuple('Stack', '')):
+    @classmethod
+    def load(cls, config):
+        return cls()
+
+
+class Environment(namedtuple('Environment', '')):
+    @classmethod
+    def load(cls, config):
+        return cls()
+
+
+class Blueprint(namedtuple('Blueprint', '')):
+    @classmethod
+    def load(cls, config):
+        return cls()
+
--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -1,14 +1,28 @@
 # -*- encoding: utf-8 -*-
 
 import pytest
+import yaml
 from awscfncli import config
 
 
+@pytest.fixture(scope='session')
+def dummy_config(tmpdir_factory):
+    c = {
+        'Version': 2,
+        'Blueprints': {},
+        'Environments': {}
+    }
+    fn = tmpdir_factory.mktemp('data').join('img.png')
+    with open(fn, 'w') as fp:
+        yaml.safe_dump(c, fp)
+    return fn
+
+
 class TestConfig(object):
-    def test_load(self):
-        c = config.load('')
+    def test_load(self, dummy_config):
+        c = config.load(dummy_config)
         assert c is not None
 
-    def test_validation(self):
-        c = config.load('')
-        assert c.validate() == False
+        assert c.version == 2
+        assert c.blueprints == {}
+        assert c.environments == {}
