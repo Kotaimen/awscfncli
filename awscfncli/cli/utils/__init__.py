5d8b22572a76d026da9e9ad0ca766b8758f57836
Kotaimen <kotaimen.c@gmail.com>
2018-03-15 14:55:39 -0400

    Supports tailing stack events.

--- awscfncli/cli/utils/__init__.py
+++ awscfncli/cli/utils/__init__.py
@@ -3,3 +3,4 @@ from .pprint import echo_pair, pretty_print_stack, STACK_STATUS_TO_COLOR, \
 from .deco import boto3_exception_handler
 from .pager import custom_paginator
 from .context import ContextObject
+from .events import tail_stack_events, start_tail_stack_events_daemon
