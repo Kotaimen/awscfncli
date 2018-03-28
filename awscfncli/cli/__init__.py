ee7626a79ed2c37590a3513a5fd697db49f0f2d7
Kotaimen <kotaimen.c@gmail.com>
2018-03-29 00:54:04 +0800

    Temporary disable stack commands (working on changeset)

--- awscfncli/cli/__init__.py
+++ awscfncli/cli/__init__.py
@@ -1,12 +1,11 @@
 # Import all commands here so they get registered in Click
 
 from .main import cfn_cli
-from .stack import stack
-
-from .stack.describe import describe
-from .stack.deploy import deploy
-from .stack.delete import delete
-from .stack.update import update
-from .stack.tail import tail
+# from .stack import stack
+# from .stack.describe import describe
+# from .stack.deploy import deploy
+# from .stack.delete import delete
+# from .stack.update import update
+# from .stack.tail import tail
 
 from .changeset import sync
