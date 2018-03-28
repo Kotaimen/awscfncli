422539466ee995671d845b60e0913b326606b950
Kotaimen <kotaimen.c@gmail.com>
2018-03-29 01:00:21 +0800

    Move stage/stack from arguments to options, and update context object accordingly

--- awscfncli/cli/main.py
+++ awscfncli/cli/main.py
@@ -18,48 +18,62 @@ VERSION = pkg_resources.require('awscfncli')[0].version
               type=click.Path(exists=False, dir_okay=True),
               default='cfn-cli.yml',
               help='Alternate stack configuration file '
-                   '(default: cfn-cli.yml)',
-              )
+                   '(default: cfn-cli.yml)')
+@click.option('-t', '--stage',
+              type=click.STRING, default='Default',
+              help='Specify a deployment stage name or pattern'
+                   '(default: Default)')
+@click.option('-s', '--stack',
+              type=click.STRING, default='*',
+              help='Specify a stack name or pattern'
+                   '(default: *)')
 @click.option('--profile',
-              type=click.STRING,
-              default=None,
+              type=click.STRING, default=None,
               help='Profile name from AWS credential file, overrides '
-                   'config/env setting.'
-              )
+                   'config/env setting.')
 @click.option('--region',
-              type=click.STRING,
-              default=None,
-              help='AWS region to use, overrides config/env setting.'
-              )
+              type=click.STRING, default=None,
+              help='AWS region to use, overrides config/env setting.')
+@click.option('-1', '--one',
+              is_flag=True, default=False,
+              help='Execute command on first matching ')
 @click.option('-v', '--verbose', count=True,
               help='Be more verbose.')
-def cfn_cli(ctx, file, profile, region, verbose):
+def cfn_cli(ctx, file, stage, stack, profile, region, one, verbose):
     """AWS CloudFormation stack management command line interface.
 
-    By default, cfn-cli try to find "cfn-cli.yml" in
-    current directory and load environment and stack configurations.
+    Stages and stacks can be selected using globs:
+
+    \b
+      cfn-cli --stack=DDB* stack deploy
+
+    Options can be specified using environment variables:
+
+    \b
+      CFNCLI_STACK=DDB cfn-cli stack deploy
 
     Usage:
 
     \b
-      cfn-cli [-f config_file_or_dir] [OPTIONS...] COMMAND SUBCOMMAND [ARGS...] ENVIRONMENT_NAME STACK_NAME
+      cfn-cli [OPTIONS...] COMMAND SUBCOMMAND [ARGS...]
       cfn-cli --help
 
     Some examples:
 
     \b
-      cfn-cli stack deploy Production Networking
-      cfn-cli -f foobar/MyGreatProject.yml stack deploy 'Prod*' 'Net*'
-
-    \b
       cfn-cli stack deploy --help
+      cfn-cli -t dev -s ddb stack deploy
+      cfn-cli -f foobar/MyGreatProject.yml stack deploy
 
     """
 
     ctx_obj = ContextObject(
         config_file=file,
+        stage_pattern=stage,
+        stack_pattern=stack,
         profile=profile,
         region=region,
+        first_stack=one,
         verbosity=verbose,
     )
 
--- awscfncli/cli/utils/context.py
+++ awscfncli/cli/utils/context.py
@@ -10,56 +10,114 @@ from ...config import load_config, ConfigError
 
 
 class ContextObject(object):
-    """Click context object """
+    """Click context object"""
 
     def __init__(self,
                  config_file,
+                 stage_pattern,
+                 stack_pattern,
                  profile,
                  region,
+                 first_stack,
                  verbosity):
-        self.config = None
+
         self.config_file = config_file
+        self.stage_pattern = stage_pattern
+        self.stack_pattern = stack_pattern
         self.profile = profile
         self.region = region
+        self.first_stack = first_stack
         self.verbosity = verbosity
 
-    def load(self):
-        assert self.config is None
+        # lazy initialization
+        self._config = None
+        self._stacks = None
+
+    @property
+    def config(self):
+        """Config object
+
+        Layout:
+
+            Config {
+                Stages {
+                    Stacks {
+                        StackConfig {
+                            Stack Parameter
+                            ...
+                            Metadata {
+                                Stack Metadata
+                                ...
+                            }
+                        }
+                    }
+                }
+            }
+        """
+        if self._config is None:
+            self.load_config()
+        return self._config
+
+    @property
+    def stacks(self):
+        """Matching stack configs"""
+        if self._stacks is None:
+            self.find_stacks()
+        return self._stacks
+
+    def load_config(self):
+        """Load config using given context"""
+
         if not os.path.exists(self.config_file):
             raise ConfigError(
-                'Stack configuration file not found: {}, specify a non-default '
-                'filename using -f '.format(self.config_file))
-        self.config = load_config(self.config_file)
+                'Stack configuration file not found: "{}", specify a '
+                'non-default filename using -f.'.format(self.config_file))
+        self._config = load_config(self.config_file)
 
-    def find_stack_config(self, env_pattern, stack_pattern):
-        """ Find matching stack configurations
+    def find_stacks(self):
+        """Find all matching stacks"""
+        stack_configs = list(self._find_stack_config())
 
-        Assuming config is a dict of dict of namedtuples:
-            config[env][stack] = stack_config
+        if not stack_configs:
+            raise ConfigError(
+                'No stack matching specified pattern {}.{}, '.format(
+                    self.stage_pattern, self.stack_pattern) +
+                'possible values are: \n' +
+                '\n'.join(self._find_all_stacks())
+            )
+
+        self._stacks = list()
+        for n, stack_config in enumerate(stack_configs):
+            if n > 0 and self.first_stack: return
+            # make a deep copy as config may be modified in commands
+            stack_config = copy.deepcopy(stack_config)
+
+            # override parameters
+            if self.profile is not None:
+                stack_config['Metadata']['Profile'] = self.profile
+            if self.region is not None:
+                stack_config['Metadata']['Region'] = self.region
+
+            self._stacks.append(stack_config)
+
+    # XXX should be put into config package
+    def _find_all_stacks(self):
+        for stage_name in self.config.list_environments():
+            for stack_name in self.config.list_stacks(stage_name):
+                yield '.'.join([stage_name, stack_name])
+
+    # XXX should be put into config package
+    def _find_stack_config(self):
+        """All stack config matching stage/stack patterns
+
         """
-        for env_name in self.config.list_environments():
-            if fnmatch.fnmatchcase(env_name, env_pattern):
-                for stack_name in self.config.list_stacks(env_name):
-                    if fnmatch.fnmatchcase(stack_name, stack_pattern):
+        for stage_name in self.config.list_environments():
+            if fnmatch.fnmatchcase(stage_name, self.stage_pattern):
+                for stack_name in self.config.list_stacks(stage_name):
+                    if fnmatch.fnmatchcase(stack_name, self.stack_pattern):
                         stack_config = \
-                            self.config.get_stack(env_name, stack_name)
-                        # make a deep copy as config may be modified in commands
-                        stack_config = copy.deepcopy(stack_config)
-
-                        # override parameters
-                        if self.profile is not None:
-                            stack_config['Metadata']['Profile'] = self.profile
-                        if self.region is not None:
-                            stack_config['Metadata']['Region'] = self.region
-
+                            self.config.get_stack(stage_name, stack_name)
                         yield stack_config
-
-    def find_one_stack_config(self, env_pattern, stack_pattern):
-        for r in self.find_stack_config(env_pattern, stack_pattern):
-            return r
-        else:
-            raise ConfigError('Cannot find stack with name "{}" '
-                              'in environment "{}".'.format(stack_pattern, env_pattern))
 
     def get_boto3_session(self, stack_config):
 
