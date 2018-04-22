4560f9ce4440aa0ad981c7a09817b99ca38767d6
Kotaimen <kotaimen.c@gmail.com>
2018-04-23 00:11:18 +0800

    Add SAM sample config and templates.

---
+++ samples/SAM/README.md
@@ -0,0 +1,5 @@
+# SAM
+
+Contains sample `cfn-cli` config files for 
+[official SAM repository](https://github.com/awslabs/serverless-application-model/tree/master/examples).
+
--- samples/SAM/api_backend/src/index.js
+++ samples/SAM/api_backend/src/index.js
@@ -7,7 +7,7 @@ const dynamo = new AWS.DynamoDB.DocumentClient();
 const tableName = process.env.TABLE_NAME;
 
 const createResponse = (statusCode, body) => {
-
+    
     return {
         statusCode: statusCode,
         body: body
@@ -15,16 +15,16 @@ const createResponse = (statusCode, body) => {
 };
 
 exports.get = (event, context, callback) => {
-
+    
     let params = {
         TableName: tableName,
         Key: {
             id: event.pathParameters.resourceId
         }
     };
-
+    
     let dbGet = (params) => { return dynamo.get(params).promise() };
-
+    
     dbGet(params).then( (data) => {
         if (!data.Item) {
             callback(null, createResponse(404, "ITEM NOT FOUND"));
@@ -32,14 +32,14 @@ exports.get = (event, context, callback) => {
         }
         console.log(`RETRIEVED ITEM SUCCESSFULLY WITH doc = ${data.Item.doc}`);
         callback(null, createResponse(200, data.Item.doc));
-    }).catch( (err) => {
+    }).catch( (err) => { 
         console.log(`GET ITEM FAILED FOR doc = ${params.Key.id}, WITH ERROR: ${err}`);
         callback(null, createResponse(500, err));
     });
 };
 
 exports.put = (event, context, callback) => {
-
+    
     let item = {
         id: event.pathParameters.resourceId,
         doc: event.body
@@ -49,20 +49,20 @@ exports.put = (event, context, callback) => {
         TableName: tableName,
         Item: item
     };
-
+    
     let dbPut = (params) => { return dynamo.put(params).promise() };
-
+    
     dbPut(params).then( (data) => {
         console.log(`PUT ITEM SUCCEEDED WITH doc = ${item.doc}`);
         callback(null, createResponse(200, null));
-    }).catch( (err) => {
+    }).catch( (err) => { 
         console.log(`PUT ITEM FAILED FOR doc = ${item.doc}, WITH ERROR: ${err}`);
-        callback(null, createResponse(500, err));
+        callback(null, createResponse(500, err)); 
     });
 };
 
 exports.delete = (event, context, callback) => {
-
+    
     let params = {
         TableName: tableName,
         Key: {
@@ -70,9 +70,9 @@ exports.delete = (event, context, callback) => {
         },
         ReturnValues: 'ALL_OLD'
     };
-
+    
     let dbDelete = (params) => { return dynamo.delete(params).promise() };
-
+    
     dbDelete(params).then( (data) => {
         if (!data.Attributes) {
             callback(null, createResponse(404, "ITEM NOT FOUND FOR DELETION"));
@@ -80,7 +80,7 @@ exports.delete = (event, context, callback) => {
         }
         console.log(`DELETED ITEM SUCCESSFULLY WITH id = ${event.pathParameters.resourceId}`);
         callback(null, createResponse(200, null));
-    }).catch( (err) => {
+    }).catch( (err) => { 
         console.log(`DELETE ITEM FAILED FOR id = ${event.pathParameters.resourceId}, WITH ERROR: ${err}`);
         callback(null, createResponse(500, err));
     });
