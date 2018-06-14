dda91bb2445a90ac16e87c7095390cd89bd29764
Ray <gliese.q@gmail.com>
2018-06-15 01:45:43 +0800

    Bump up version

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     name='awscfncli2',
 
     # Version and description
-    version='2.0.0-rc2',
+    version='2.0.0',
     description='AWS CloudFormation CLI',
     long_description=long_description,
 
