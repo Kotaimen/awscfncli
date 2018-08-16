ca1174ca59b25c5485ec9603d17951e353a23683
Cyril Cao <lcao@irobot.com>
2018-08-16 12:05:15 +0800

    Add more base images and test validate command to trigger config parsing

--- docker/build_all.sh
+++ docker/build_all.sh
@@ -1,13 +1,28 @@
 #!/usr/bin/env bash
 
-docker build -t awscfncli2-python2 . -f docker/Dockerfile-python2.yml
-docker run awscfncli2-python3 --version
+set -e
 
-docker build -t awscfncli2-python3 . -f docker/Dockerfile-python3.yml
-docker run awscfncli2-python3 --version
+docker build -t awscfncli2-python27 . -f docker/Dockerfile-python27.yml
+docker run --rm -v ~/.aws:/root/.aws:ro -e AWS_PROFILE=${AWS_PROFILE} awscfncli2-python27  \
+    -v -f /usr/src/app/samples/sample-confg.yml -s Staging.DDBTable1 validate
+
+docker build -t awscfncli2-python36 . -f docker/Dockerfile-python36.yml
+docker run --rm -v ~/.aws:/root/.aws:ro -e AWS_PROFILE=${AWS_PROFILE} awscfncli2-python36  \
+    -v -f /usr/src/app/samples/sample-confg.yml -s Staging.DDBTable1 validate
+
+docker build -t awscfncli2-python37 . -f docker/Dockerfile-python37.yml
+docker run --rm -v ~/.aws:/root/.aws:ro -e AWS_PROFILE=${AWS_PROFILE} awscfncli2-python37  \
+    -v -f /usr/src/app/samples/sample-confg.yml -s Staging.DDBTable1 validate
 
 docker build -t awscfncli2-amazonlinux-2017.09 . -f docker/Dockerfile-amazonlinux-2017.09.yml
-docker run awscfncli2-amazonlinux-2017.09 --version
+docker run --rm -v ~/.aws:/root/.aws:ro -e AWS_PROFILE=${AWS_PROFILE} awscfncli2-amazonlinux-2017.09  \
+    -v -f /usr/src/app/samples/sample-confg.yml -s Staging.DDBTable1 validate
+
+docker build -t awscfncli2-amazonlinux-2018.03 . -f docker/Dockerfile-amazonlinux-2018.03.yml
+docker run --rm -v ~/.aws:/root/.aws:ro -e AWS_PROFILE=${AWS_PROFILE} awscfncli2-amazonlinux-2018.03  \
+    -v -f /usr/src/app/samples/sample-confg.yml -s Staging.DDBTable1 validate
 
 docker build -t awscfncli2-alpine . -f docker/Dockerfile-alpine.yml
-docker run awscfncli2-alpine --version
+docker run --rm -v ~/.aws:/root/.aws:ro -e AWS_PROFILE=${AWS_PROFILE} awscfncli2-alpine  \
+    -v -f /usr/src/app/samples/sample-confg.yml -s Staging.DDBTable1 validate
+
