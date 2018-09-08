251e5cdb3bf0d1dc951f07a1104f7c37c815ea06
Kotaimen <kotaimen.c@gmail.com>
2018-09-09 00:35:44 +0800

    Add runner package

---
+++ awscfncli2/runner/__init__.py
@@ -0,0 +1,3 @@
+from .stack_selector import StackSelector
+from .boto3_profile import Boto3Profile
+
---
+++ awscfncli2/runner/boto3_profile.py
@@ -0,0 +1,28 @@
+import boto3
+
+
+class Boto3Profile(object):
+    """Store parameters for creating a boto3 profile"""
+
+    def __init__(self, profile_name, region_name):
+        self.profile_name = profile_name
+        self.region_name = region_name
+
+    def update(self, other):
+        """Update profile with value from another one"""
+        assert isinstance(other, Boto3Profile)
+        if other.profile_name:
+            self.profile_name = other.profile_name
+        if other.region_name:
+            self.region_name = other.region_name
+
+    def get_boto3_sessoin(self):
+        # XXX: caching session object?
+        return boto3.Session(
+            profile_name=self.profile_name,
+            region_name=self.region_name,
+        )
+
+    def __repr__(self):
+        return '{}({}/{})'.format(self.__class__.__name__, self.profile_name,
+                                  self.region_name)
