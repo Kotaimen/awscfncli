72950379ad34bbb4c45664f93633b729c7b1f512
Kotaimen <kotaimen.c@gmail.com>
2018-09-11 03:17:10 -0400

    Move pretty print IF to StackPrettyPrinter

--- awscfncli2/cli/utils/pprint.py
+++ awscfncli2/cli/utils/pprint.py
@@ -1,6 +1,10 @@
 import click
 import yaml
 
+from .colormaps import CHANGESET_STATUS_TO_COLOR, ACTION_TO_COLOR, \
+    STACK_STATUS_TO_COLOR
+from .events import start_tail_stack_events_daemon
+
 
 def echo_pair(key, value=None, indent=0,
               value_style=None, key_style=None,
@@ -30,117 +34,96 @@ def echo_pair(key, value=None, indent=0,
             click.secho(value, **value_style)
 
 
-def echo_pair_if_exists(data, key, value, indent=2, key_style=None, value_style=None):
+def echo_pair_if_exists(data, key, value, indent=2, key_style=None,
+                        value_style=None):
     if value in data:
         echo_pair(key, data[value], indent=indent,
                   key_style=key_style, value_style=value_style, )
 
 
-def pretty_print_config(qualified_name, stack_config, session, verbosity=0,
-                        retrieve_identity=False):
-    """Pretty stack config"""
-    click.secho(qualified_name, bold=True)
+class StackPrettyPrinter(object):
+    """Pretty print stack parameter, status and events"""
 
-    echo_pair('Profile', session.profile_name)
-    echo_pair('Region', session.region_name)
+    def __init__(self, verbosity=0):
+        self.verbosity = verbosity
 
-    # following requires dry run
-    if retrieve_identity:
-        sts = session.client('sts')
-        identity = sts.get_caller_identity()
-        echo_pair('Account', identity['Account'])
-        echo_pair('Identity', identity['Arn'])
+    def pprint_stack_name(self, qualified_name):
+        """Print stack qualifid name"""
+        click.secho(qualified_name, bold=True)
 
-    if verbosity > 0:
-        click.secho(
-            yaml.safe_dump(stack_config, default_flow_style=False),
-            fg='white', dim=True
-        )
+    def pprint_session(self, session, retrieve_identity=True):
+        """Print boto3 session"""
+        echo_pair('Profile', session.profile_name)
+        echo_pair('Region', session.region_name)
 
-    echo_pair('Stack Name', stack_config['StackName'])
+        if retrieve_identity:
+            sts = session.client('sts')
+            identity = sts.get_caller_identity()
+            echo_pair('Account', identity['Account'])
+            echo_pair('Identity', identity['Arn'])
 
+    def pprint_stack_metadata(self, metadata):
+        """Print stack metadata"""
+        if self.verbosity > 0:
+            click.secho(
+                yaml.safe_dump(metadata,
+                               default_flow_style=False),
+                fg='white', dim=True
+            )
 
-def pretty_print_stack(stack, detail=False):
-    """Pretty print stack status"""
-    echo_pair('Stack ID', stack.stack_id)
+    def pprint_stack_parameters(self, parameters):
+        """Print stack parameters"""
+        if self.verbosity > 0:
+            click.secho(
+                yaml.safe_dump(parameters,
+                               default_flow_style=False),
+                fg='white', dim=True
+            )
 
-    if not detail:
-        return
+    def pprint_stack(self, stack, detail=False):
+        """Pretty print stack status"""
+        echo_pair('Stack ID', stack.stack_id)
 
-    # echo_pair('Name', stack.stack_name)
-    echo_pair('Description', stack.description)
+        if not detail:
+            return
 
-    echo_pair('Status', stack.stack_status,
-              value_style=STACK_STATUS_TO_COLOR[stack.stack_status])
-    echo_pair('Created', stack.creation_time)
-    if stack.last_updated_time:
-        echo_pair('Last Updated', stack.last_updated_time)
-    echo_pair('Capabilities', stack.capabilities)
-    echo_pair('Termination Protection', stack.enable_termination_protection)
+        # echo_pair('Name', stack.stack_name)
+        echo_pair('Description', stack.description)
 
-    if stack.parameters:
-        echo_pair('Parameters')
-        for p in stack.parameters:
-            if 'ResolvedValue' in p:
-                # SSM parameter
-                echo_pair('%s (%s)' % (p['ParameterKey'], p['ParameterValue']),
-                          p['ResolvedValue'], indent=2)
-            else:
-                echo_pair(p['ParameterKey'], p['ParameterValue'], indent=2)
-    if stack.outputs:
-        echo_pair('Outputs')
-        for o in stack.outputs:
-            echo_pair(o['OutputKey'], o['OutputValue'], indent=2)
-    if stack.tags:
-        echo_pair('Tags')
-        for t in stack.tags:
-            echo_pair(t['Key'], t['Value'], indent=2)
+        echo_pair('Status', stack.stack_status,
+                  value_style=STACK_STATUS_TO_COLOR[stack.stack_status])
+        echo_pair('Created', stack.creation_time)
+        if stack.last_updated_time:
+            echo_pair('Last Updated', stack.last_updated_time)
+        echo_pair('Capabilities', stack.capabilities)
+        echo_pair('Termination Protection', stack.enable_termination_protection)
 
+        if stack.parameters:
+            echo_pair('Parameters')
+            for p in stack.parameters:
+                if 'ResolvedValue' in p:
+                    # SSM parameter
+                    echo_pair(
+                        '%s (%s)' % (p['ParameterKey'], p['ParameterValue']),
+                        p['ResolvedValue'], indent=2)
+                else:
+                    echo_pair(p['ParameterKey'], p['ParameterValue'], indent=2)
+        if stack.outputs:
+            echo_pair('Outputs')
+            for o in stack.outputs:
+                echo_pair(o['OutputKey'], o['OutputValue'], indent=2)
+        if stack.tags:
+            echo_pair('Tags')
+            for t in stack.tags:
+                echo_pair(t['Key'], t['Value'], indent=2)
 
-#
-# Status string to click.style mapping
-#
-STACK_STATUS_TO_COLOR = {
-    'CREATE_IN_PROGRESS': dict(fg='yellow'),
-    'CREATE_FAILED': dict(fg='red'),
-    'CREATE_COMPLETE': dict(fg='green'),
-    'ROLLBACK_IN_PROGRESS': dict(fg='yellow'),
-    'ROLLBACK_FAILED': dict(fg='red'),
-    'ROLLBACK_COMPLETE': dict(fg='red'),
-    'DELETE_IN_PROGRESS': dict(fg='yellow'),
-    'DELETE_FAILED': dict(fg='red'),
-    'DELETE_SKIPPED': dict(fg='white', dim=True),
-    'DELETE_COMPLETE': dict(fg='white', dim=True),
-    'UPDATE_IN_PROGRESS': dict(fg='yellow'),
-    'UPDATE_COMPLETE_CLEANUP_IN_PROGRESS': dict(fg='green'),
-    'UPDATE_COMPLETE': dict(fg='green'),
-    'UPDATE_ROLLBACK_IN_PROGRESS': dict(fg='red'),
-    'UPDATE_ROLLBACK_FAILED': dict(fg='red'),
-    'UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS': dict(fg='red'),
-    'UPDATE_ROLLBACK_COMPLETE': dict(fg='green'),
-    'UPDATE_FAILED': dict(fg='red'),
-    'REVIEW_IN_PROGRESS': dict(fg='yellow'),
-    # custom status:
-    'STACK_NOT_FOUND': dict(fg='red')
-}
+    def wait_until_deploy_complete(self, session, stack):
+        # start event tailing
+        start_tail_stack_events_daemon(session, stack, latest_events=0)
 
-CHANGESET_STATUS_TO_COLOR = {
-    'UNAVAILABLE': dict(fg='white', dim=True),
-    'AVAILABLE': dict(fg='green'),
-    'EXECUTE_IN_PROGRESS': dict(fg='yellow'),
-    'EXECUTE_COMPLETE': dict(fg='green'),
-    'EXECUTE_FAILED': dict(fg='red'),
-    'OBSOLETE': dict(fg='white', dim=True),
+        # wait until update complete
+        waiter = session.client('cloudformation').get_waiter(
+            'stack_create_complete')
+        waiter.wait(StackName=stack.stack_id)
 
-    'CREATE_PENDING': dict(fg='white', dim=True),
-    'CREATE_IN_PROGRESS': dict(fg='yellow'),
-    'CREATE_COMPLETE': dict(fg='green'),
-    'DELETE_COMPLETE': dict(fg='white', dim=True),
-    'FAILED': dict(fg='red'),
-}
-
-ACTION_TO_COLOR = {
-    'Add': dict(fg='green'),
-    'Modify': dict(fg='yellow', dim=True),
-    'Remove': dict(fg='red'),
-}
+        click.secho('Stack deployment complete.', fg='green')
