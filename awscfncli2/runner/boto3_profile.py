c2d64f8a81441d0a8e390c1420829b08e339aa08
Kotaimen <kotaimen.c@gmail.com>
2018-09-19 15:31:33 +0800

    Cache session object so complex deployment runs faster.

--- awscfncli2/runner/boto3_profile.py
+++ awscfncli2/runner/boto3_profile.py
@@ -1,8 +1,12 @@
+import threading
 import boto3
 
 
 class Boto3Profile(object):
     """Store parameters for creating a boto3 profile"""
+
+    session_cache = dict()
+    lock = threading.Lock()
 
     def __init__(self, profile_name, region_name):
         self.profile_name = profile_name
@@ -17,11 +21,16 @@ class Boto3Profile(object):
             self.region_name = other.region_name
 
     def get_boto3_session(self):
-        # XXX: caching session object?
-        return boto3.Session(
-            profile_name=self.profile_name,
-            region_name=self.region_name,
-        )
+        session_key = self.region_name, self.profile_name
+        # cache the session object as the majority of stacks
+        #   are staying in one region
+        with self.lock:
+            if session_key not in self.session_cache:
+                self.session_cache[session_key] = boto3.Session(
+                    profile_name=self.profile_name,
+                    region_name=self.region_name,
+                )
+            return self.session_cache[session_key]
 
     def __repr__(self):
         return '{}({}/{})'.format(self.__class__.__name__, self.profile_name,
