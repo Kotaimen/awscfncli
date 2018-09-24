ffd24580ed57f70984c269403fbd54ed078f451f
Ray <gliese.q@gmail.com>
2018-09-24 15:26:31 +0800

    Use tuple instead of StackKey for stack index

--- awscfncli2/config/deployment.py
+++ awscfncli2/config/deployment.py
@@ -98,24 +98,24 @@ class Deployment(object):
         self._index = dict()
 
     def add_stack(self, stage_key, stack_key, stack):
-        key = StackKey(stage_key, stack_key)
+        key = (stage_key, stack_key)
         self._index[key] = stack
 
     def get_stack(self, stage_key, stack_key):
-        key = StackKey(stage_key, stack_key)
+        key = (stage_key, stack_key)
         return self._index.get(key)
 
     def get_stacks(self, stage_key):
-        return list(k for k in self._index if k.stage_key == stage_key)
+        return list(k1 for (k1, k2) in self._index if k1 == stage_key)
 
     def query_stacks(self, stage_pattern='*', stack_pattern='*'):
         """Find all stack config matching stage/stack patterns
         """
         result = list()
-        for key in self._index:
-            if fnmatch.fnmatchcase(key.StageKey, stage_pattern) \
-                    and fnmatch.fnmatchcase(key.StackKey, stack_pattern):
-                result.append(self._index[key])
+        for (k1, k2) in self._index:
+            if fnmatch.fnmatchcase(k1, stage_pattern) \
+                    and fnmatch.fnmatchcase(k2, stack_pattern):
+                result.append(self._index[(k1, k2)])
 
         result.sort(key=lambda stack: stack.metadata.Order)
 
