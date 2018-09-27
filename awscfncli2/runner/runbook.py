9a7306d362643488b485a3c12a8d8853a1a89c88
Kotaimen <kotaimen.c@gmail.com>
2018-09-27 17:28:28 +0800

    Supports ResourceTypes (part of #39)

--- awscfncli2/runner/runbook.py
+++ awscfncli2/runner/runbook.py
@@ -1,114 +1,10 @@
 import os
 import threading
-from collections import OrderedDict
 
-import six
-
+from .boto3_params import make_boto3_parameters
 from .boto3_profile import Boto3Profile
 from .package import package_template
-from ..config import ConfigError, StackDeployment, CANNED_STACK_POLICIES
-
-
-def _normalize_value(v):
-    if isinstance(v, bool):
-        return 'true' if v else 'false'
-    elif isinstance(v, int):
-        return str(v)
-    else:
-        return v
-
-
-def _make_boto3_parameters(parameters, is_packaging):
-    # inject parameters
-    StackName = parameters.StackName
-
-    Template = parameters.Template
-    if Template.startswith('https') or Template.startswith('http'):
-        # s3 template
-        TemplateURL, TemplateBody = Template, None
-    elif is_packaging:
-        # local template with package=on
-        TemplateURL = Template
-        TemplateBody = None
-    else:
-        # local template
-        TemplateURL = None
-        with open(Template) as fp:
-            TemplateBody = fp.read()
-
-    # lookup canned policy
-    StackPolicy = parameters.StackPolicy
-    if StackPolicy is not None:
-        try:
-            StackPolicyBody = CANNED_STACK_POLICIES[StackPolicy]
-        except KeyError:
-            # treat as local file
-            if os.path.exists(StackPolicy) and os.path.isfile(StackPolicy):
-                try:
-                    with open(StackPolicy) as fp:
-                        StackPolicyBody = fp.read()
-                except Exception as ex:
-                    raise ConfigError(
-                        'Error reading stack policy {}'.format(StackPolicy))
-            else:
-                raise ConfigError(
-                    'Invalid stack policy, either specify a canned policy name '
-                    'or a local policy document.')
-    else:
-        StackPolicyBody = None
-    StackPolicyURL = None
-
-    # Normalize parameter config
-    Parameters = parameters.Parameters
-    normalized_params = None
-    if Parameters and isinstance(Parameters, dict):
-        normalized_params = list(
-            {
-                'ParameterKey': k,
-                'ParameterValue': _normalize_value(v)
-            }
-            for k, v in
-            six.iteritems(OrderedDict(
-                sorted(six.iteritems(Parameters)))
-            )
-        )
-
-    # Normalize tag config
-    Tags = parameters.Tags
-    normalized_tags = None
-    if Tags and isinstance(Tags, dict):
-        normalized_tags = list(
-            {'Key': k, 'Value': v}
-            for k, v in
-            six.iteritems(OrderedDict(
-                sorted(six.iteritems(Tags)))
-            )
-        )
-
-    normalized_config = dict(
-        StackName=StackName,
-        TemplateURL=TemplateURL,
-        TemplateBody=TemplateBody,
-        DisableRollback=parameters.DisableRollback,
-        RollbackConfiguration=parameters.RollbackConfiguration,
-        TimeoutInMinutes=parameters.TimeoutInMinutes,
-        NotificationARNs=parameters.NotificationARNs,
-        Capabilities=parameters.Capabilities,
-        ResourceTypes=parameters.ResourceTypes,
-        RoleARN=parameters.RoleARN,
-        OnFailure=parameters.OnFailure,
-        StackPolicyBody=StackPolicyBody,
-        StackPolicyURL=StackPolicyURL,
-        Parameters=normalized_params,
-        Tags=normalized_tags,
-        ClientRequestToken=parameters.ClientRequestToken,
-        EnableTerminationProtection=parameters.EnableTerminationProtection,
-    )
-
-    # drop all None and empty list
-    return dict(
-        (k, v) for k, v in six.iteritems(normalized_config) if
-        v is not None)
+from ..config import StackDeployment
 
 
 def is_local_path(path):
@@ -130,7 +26,7 @@ class StackDeploymentContext(object):
 
         self._stack_key = stack_deployment.stack_key.qualified_name
         self._metadata = stack_deployment.metadata._asdict()
-        self._parameters = _make_boto3_parameters(
+        self._parameters = make_boto3_parameters(
             stack_deployment.parameters, self.metadata['Package'])
 
     @property
