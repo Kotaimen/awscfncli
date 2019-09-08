340b9f9756ddffe0778348c5744852c6c50e0798
Kotaimen <kotaimen.c@gmail.com>
2019-09-08 15:13:57 +0800

    Update dev infra to Pipenv, add code quality tools.

---
+++ Makefile
@@ -0,0 +1,19 @@
+# Simplify build management
+
+env:
+	pipenv sync --dev
+
+test:
+	pytest --cov awscfncli2 tests/unit
+
+dev: lint test
+
+lint:
+	flake8
+	bandit -r awscfncli2 -c bandit
+
+black:
+	black awscfncli2/* tests/*
+
+black-check:
+	black --check awscfncli2/* tests/*
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 
 import os
+
 from setuptools import setup, find_packages
 
 __author__ = 'kotaimen'
@@ -8,50 +9,23 @@ __date__ = '22-Feb-2018'
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-long_description = """
-
-AWS CloudFormation CLI
-**********************
-
-Introduction
-============
-
-``awscfncli`` AWS CloudFormation stack management command line interface.
-
-Features:
-
-* awscli a-like CLI interface.
-* Simple YAML stacks configuration file.
-* Tracking stack events in the CLI.
-* Managing stacks across different stages and accounts.
-
-"""
-
+long_description = open('README.md').read()
 
 install_requires = [
     'botocore',
     'boto3',
-    'awscli>=1.16.0'
-    'six>=1.10.0',
-    'click>=6.0,<8',
-    'PyYAML>=3.10',
+    'awscli',
+    'click~=7.0',
+    'PyYAML>=5.1',
     'jsonschema>=2.6.0'
 ]
-
-test_requires = [
-    'pytest',
-    'pytest-cov',
-    'mock'
-]
-
-dev_requires = test_requires
 
 setup(
     # Project Name
     name='awscfncli2',
 
     # Version and description
-    version='2.1.18',
+    version='3.0.0rc0',
     description='AWS CloudFormation CLI',
     long_description=long_description,
 
@@ -65,42 +39,28 @@ setup(
     # License detail
     license='MIT',
 
-    # Classification and Keywords
+    # Classification and Keyw1ords
     classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-
-        'License :: OSI Approved :: MIT License',
-
-        'Operating System :: OS Independent',
-
         'Development Status :: 5 - Production/Stable',
-
-        'Natural Language :: English',
-
+        'Environment :: Console',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
-
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Build Tools',
         'Topic :: Utilities',
     ],
-    keywords='awscfncli awscfncli2 aws cfn cli cloudformation stack '
-             'template changeset commandline development sam serverless',
-
+    keywords='aws cfn cli awscfncli cloudformation changeset sam serverless',
+    python_requires=">=3.6",
     packages=find_packages(exclude=['tests.*', 'tests']),
     package_data={
-        'awscfncli2.config': ['*.json', '*.yaml']
-
+        # 'awscfncli2.config': ['*.json', '*.yaml']
     },
     install_requires=install_requires,
-    extras_require={
-        'test': test_requires,
-        'dev': dev_requires
-    },
-
     entry_points='''
-        [console_scripts]
-        cfn-cli=awscfncli2.__main__:main
+    [console_scripts]
+    cfn-cli=awscfncli2.__main__:main
     ''',
 )
