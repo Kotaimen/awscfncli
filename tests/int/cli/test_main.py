ae7ae74413376f7919a3c23a6251d6b9141df1d4
Kotaimen <kotaimen.c@gmail.com>
2019-11-09 22:25:46 +0800

    Complete int test for cli commands.

--- tests/int/cli/test_main.py
+++ tests/int/cli/test_main.py
@@ -1,15 +1,25 @@
-"""Test main command"""
+import os
+from unittest.mock import patch
 import click.testing
 
 from awscfncli2.cli.main import cli
+from awscfncli2.cli.context import Context
 
 
 def test_main():
 
-    runner = click.testing.CliRunner()
+    cli_runner = click.testing.CliRunner()
 
-    with runner.isolated_filesystem():
+    with cli_runner.isolated_filesystem():
         # by default, print help when invoked with no options
-        result = runner.invoke(cli)
+        result = cli_runner.invoke(cli)
         assert result.exit_code == 0
-        assert 'AWS CloudFormation CLI' in result.outputA
+        assert "AWS CloudFormation CLI" in result.output
+        # Generate a sample config
+        cli_runner.invoke(cli, "generate")
+        assert os.path.exists("cfn-cli.yaml")
+
+        with patch.object(Context, "runner") as mock_command_runner:
+
+            result = cli_runner.invoke(cli, "validate")
+            assert result.exit_code == 0
--- tests/int/cli/test_stack_commands.py
+++ tests/int/cli/test_stack_commands.py
@@ -1,97 +1,154 @@
-import os
 from unittest.mock import patch
 
 import click.testing
+import pytest
 
 from awscfncli2.cli.context import Context
 from awscfncli2.cli.main import cli
-from awscfncli2.runner.commands.stack_deploy_command import StackDeployCommand
-from awscfncli2.runner.commands.stack_delete_command import StackDeleteCommand
-from awscfncli2.runner.commands.stack_status_command import StackStatusCommand
-from awscfncli2.runner.commands.stack_sync_command import StackSyncCommand
-from awscfncli2.runner.commands.stack_update_command import StackUpdateCommand
+from awscfncli2.runner.commands.stack_delete_command import (
+    StackDeleteCommand,
+    StackDeleteOptions,
+)
+from awscfncli2.runner.commands.stack_deploy_command import (
+    StackDeployCommand,
+    StackDeployOptions,
+)
+from awscfncli2.runner.commands.stack_status_command import (
+    StackStatusCommand,
+    StackStatusOptions,
+)
+from awscfncli2.runner.commands.stack_sync_command import (
+    StackSyncCommand,
+    StackSyncOptions,
+)
+from awscfncli2.runner.commands.stack_update_command import (
+    StackUpdateCommand,
+    StackUpdateOptions,
+)
 
 
-@patch.object(Context, 'runner')
-def test_stack_deploy_command(mock_command_runner):
+@pytest.fixture()
+def cli_runner():
     cli_runner = click.testing.CliRunner()
-
     with cli_runner.isolated_filesystem():
-        # generate a default config file
-        result = cli_runner.invoke(cli, 'generate')
-        assert os.path.exists('cfn-cli.yaml')
+        cli_runner.invoke(cli, "generate")
+        yield cli_runner
+
+
+def test_stack_cancel_command(cli_runner):
+    with patch.object(Context, "runner") as mock_command_runner:
+        result = cli_runner.invoke(cli, "-f cfn-cli.yaml -s Develop.Table stack cancel")
         assert result.exit_code == 0
 
-        result = cli_runner.invoke(cli, 'stack deploy')
+
+def test_stack_delete_command(cli_runner):
+    with patch.object(Context, "runner") as mock_command_runner:
+        result = cli_runner.invoke(
+            cli,
+            "-f cfn-cli.yaml -s Develop.Table stack delete "
+            "--no-wait --ignore-missing",
+        )
+        assert result.exit_code == 0
+
+        command_invoked = mock_command_runner.run.call_args[0][0]
+        assert isinstance(command_invoked, StackDeleteCommand)
+
+        options_collected = command_invoked.options
+        assert isinstance(options_collected, StackDeleteOptions)
+        assert options_collected._asdict() == {
+            "no_wait": True,
+            "ignore_missing": True,
+        }
+
+
+def test_stack_deploy_command(cli_runner):
+    with patch.object(Context, "runner") as mock_command_runner:
+        result = cli_runner.invoke(
+            cli,
+            "-f cfn-cli.yaml -s Develop.Table stack deploy "
+            "--no-wait --on-failure=DO_NOTHING --disable-rollback "
+            "--timeout-in-minutes=88 --ignore-existing",
+        )
         assert result.exit_code == 0
 
         command_invoked = mock_command_runner.run.call_args[0][0]
         assert isinstance(command_invoked, StackDeployCommand)
+        options_collected = command_invoked.options
+
+        assert isinstance(options_collected, StackDeployOptions)
+        assert options_collected._asdict() == {
+            "no_wait": True,
+            "on_failure": "DO_NOTHING",
+            "disable_rollback": True,
+            "timeout_in_minutes": 88,
+            "ignore_existing": True,
+        }
 
 
-@patch.object(Context, 'runner')
-def test_stack_update_command(mock_command_runner):
-    cli_runner = click.testing.CliRunner()
+def test_status_command(cli_runner):
+    with patch.object(Context, "runner") as mock_command_runner:
+        result = cli_runner.invoke(cli, "-f cfn-cli.yaml -s Develop.Table status -re")
 
-    with cli_runner.isolated_filesystem():
-        # generate a default config file
-        result = cli_runner.invoke(cli, 'generate')
-        assert os.path.exists('cfn-cli.yaml')
+        assert result.exit_code == 0
+        command_invoked = mock_command_runner.run.call_args[0][0]
+
+        assert isinstance(command_invoked, StackStatusCommand)
+
+        options_collected = command_invoked.options
+        assert isinstance(options_collected, StackStatusOptions)
+        assert options_collected._asdict() == {
+            "dry_run": False,
+            "stack_resources": True,
+            "stack_exports": True,
+        }
+
+
+def test_stack_sync_command(cli_runner):
+    with patch.object(Context, "runner") as mock_command_runner:
+        result = cli_runner.invoke(
+            cli,
+            "-f cfn-cli.yaml -s Develop.Table stack sync "
+            "--no-wait --confirm --use-previous-template",
+        )
+
+        assert result.exit_code == 0
+        command_invoked = mock_command_runner.run.call_args[0][0]
+
+        assert isinstance(command_invoked, StackSyncCommand)
+
+        options_collected = command_invoked.options
+        assert isinstance(options_collected, StackSyncOptions)
+        assert options_collected._asdict() == {
+            "no_wait": True,
+            "confirm": True,
+            "use_previous_template": True,
+        }
+
+
+def test_stack_tail_command(cli_runner):
+    with patch.object(Context, "runner") as mock_command_runner:
+        result = cli_runner.invoke(cli, "-f cfn-cli.yaml -s Develop.Table stack tail")
         assert result.exit_code == 0
 
-        result = cli_runner.invoke(cli, 'stack update')
+
+def test_stack_update_command(cli_runner):
+    with patch.object(Context, "runner") as mock_command_runner:
+        result = cli_runner.invoke(
+            cli,
+            "-f cfn-cli.yaml -s Develop.Table stack update "
+            "--no-wait --ignore-no-update --use-previous-template "
+            "--override-policy=ALLOW_ALL",
+        )
         assert result.exit_code == 0
 
         command_invoked = mock_command_runner.run.call_args[0][0]
         assert isinstance(command_invoked, StackUpdateCommand)
 
-
-@patch.object(Context, 'runner')
-def test_stack_status_command(mock_command_runner):
-    cli_runner = click.testing.CliRunner()
-
-    with cli_runner.isolated_filesystem():
-        # generate a default config file
-        result = cli_runner.invoke(cli, 'generate')
-        assert os.path.exists('cfn-cli.yaml')
-        assert result.exit_code == 0
-
-        result = cli_runner.invoke(cli, 'status')
-        assert result.exit_code == 0
-
-        command_invoked = mock_command_runner.run.call_args[0][0]
-        assert isinstance(command_invoked, StackStatusCommand)
-
-
-@patch.object(Context, 'runner')
-def test_stack_sync_command(mock_command_runner):
-    cli_runner = click.testing.CliRunner()
-
-    with cli_runner.isolated_filesystem():
-        # generate a default config file
-        result = cli_runner.invoke(cli, 'generate')
-        assert os.path.exists('cfn-cli.yaml')
-        assert result.exit_code == 0
-
-        result = cli_runner.invoke(cli, 'stack sync')
-        assert result.exit_code == 0
-
-        command_invoked = mock_command_runner.run.call_args[0][0]
-        assert isinstance(command_invoked, StackSyncCommand)
-
-
-@patch.object(Context, 'runner')
-def test_stack_delete_command(mock_command_runner):
-    cli_runner = click.testing.CliRunner()
-
-    with cli_runner.isolated_filesystem():
-        # generate a default config file
-        result = cli_runner.invoke(cli, 'generate')
-        assert os.path.exists('cfn-cli.yaml')
-        assert result.exit_code == 0
-
-        result = cli_runner.invoke(cli, 'stack delete')
-        assert result.exit_code == 0
-
-        command_invoked = mock_command_runner.run.call_args[0][0]
-        assert isinstance(command_invoked, StackDeleteCommand)
+        options_collected = command_invoked.options
+        assert isinstance(options_collected, StackUpdateOptions)
+        assert options_collected._asdict() == {
+            "no_wait": True,
+            "ignore_no_update": True,
+            "use_previous_template": True,
+            "override_policy": "ALLOW_ALL",
+        }
